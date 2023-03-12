d51e0cbe5f06d19fc03403b56d90035ed52e0eaf
Karan Tulsani <117897987+karan151120@users.noreply.github.com>
2023-03-12 13:00:14 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    RWOnly obj = new RWOnly();
+    //obj.i = 5; // i has private access in RWOnly
+    //System.out.println(obj.i); // i has private access in RWOnly
+    
+        obj.setter(4);
+        System.out.println(obj.getter());
+  }
 }
